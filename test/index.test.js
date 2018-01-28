95ccb5d9b58484002de24c913d1ac2d872d08467
JESUS <jgamio@gmail.com>
2018-01-28 17:28:54 -0400

    Test Builder complete

--- test/index.test.js
+++ test/index.test.js
@@ -35,4 +35,65 @@ describe('Testing Hello World API', function() {
           })
     });      
 
+    describe('GET /hello', function() {
+        it("renders successfully", function(done) {
+            request(app).get('/')
+              .expect(200)
+              .expect(/hello/, done);    
+          })
+    });    
+    describe('GET /hello?uppercase=true', function() {
+        it("renders successfully", function(done) {
+            request(app).get('/?uppercase=true')
+              .expect(200)
+              .expect(/HELLO/, done);    
+          })
+    }); 
+    describe('GET /hello?reversed=true', function() {
+        it("renders successfully", function(done) {
+            request(app).get('/?reversed=true')
+              .expect(200)
+              .expect(/olleh/, done);    
+          })
+    });      
+    
+    describe('GET /hello?reversed=true&uppercase=true', function() {
+        it("renders successfully", function(done) {
+            request(app).get('/?reversed=true&uppercase=true')
+              .expect(200)
+              .expect(/OLLEH/, done);    
+          })
+    }); 
+
+
+    describe('GET /world', function() {
+        it("renders successfully", function(done) {
+            request(app).get('/')
+              .expect(200)
+              .expect(/world/, done);    
+          })
+    });    
+    describe('GET /world?uppercase=true', function() {
+        it("renders successfully", function(done) {
+            request(app).get('/?uppercase=true')
+              .expect(200)
+              .expect(/WORLD/, done);    
+          })
+    }); 
+    describe('GET /world?reversed=true', function() {
+        it("renders successfully", function(done) {
+            request(app).get('/?reversed=true')
+              .expect(200)
+              .expect(/dlrow/, done);    
+          })
+    });      
+    
+    describe('GET /world?reversed=true&uppercase=true', function() {
+        it("renders successfully", function(done) {
+            request(app).get('/?reversed=true&uppercase=true')
+              .expect(200)
+              .expect(/DLROW/, done);    
+          })
+    });  
+
 });
